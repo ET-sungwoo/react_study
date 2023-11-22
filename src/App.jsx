02212bfa2e84b8a709a2ade0181a9266253bf86e
gyomdyung <130328972+ET-sungwoo@users.noreply.github.com>
2023-11-22 15:18:34 +0900

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react';
+
+import classes from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  const addTodoHandler = (todoText) => {
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          text: todoText,
+          id: new Date().toISOString(),
+        },
+      ];
+    });
+  };
+
+  /* NewTodo */
+  const todoTextInputRef = useRef(null);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    addTodoHandler(enteredText);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={submitHandler} className={classes.form}>
+        <label htmlFor="text">Todo text</label>
+        <input type="text" id="text" ref={todoTextInputRef} />
+        <button>Add Todo</button>
+      </form>
+      {/* Todos */}
+      <ul className={classes.todos}>
+        {/* TodoItem */}
+        {todos.map((item) => (
+          <li key={item.id} className={classes.item}>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
