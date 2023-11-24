ec03fd3ee4677df81f4de3ee03584d3d683ae0a3
gyomdyung <130328972+ET-sungwoo@users.noreply.github.com>
2023-11-24 12:18:14 +0900

    2023년 11월 24일 금요일 12시 18분 14초 KST

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <header>
+        <h1>
+          <a href="/">react</a>
+        </h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/read/1">html</a>
+          </li>
+          <li>
+            <a href="/read/2">javascript</a>
+          </li>
+          <li>
+            <a href="/read/3">react</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Hello React</h2>
+
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+      </main>
+    </div>
   );
 }
 
